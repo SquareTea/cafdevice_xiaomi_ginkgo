82e754e29b67e50d3459cd7af412edcbd3982983
Pascoato <viniciuspascoato@gmail.com>
2022-10-11 13:51:57 +0000

    Revert "ginkgo: Remove obsolete debug.sf.disable_backpressure"
    - CLO still supports it, we can still benefit from disabling backpressure propagation.
    
    This reverts commit 5ac1cb39e3e59b476604cd71962a1aece694d7ed.
    
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>

--- device.mk
+++ device.mk
@@ -127,6 +127,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.sf.color_saturation=1.1
 
 PRODUCT_VENDOR_PROPERTIES += \
+    debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
     ro.config.avoid_gfx_accel=true \
