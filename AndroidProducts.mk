aryaman895/device_xiaomi_ginkgo

ginkgo: Initial ReloadedOS 13.0 bringup

AndroidProducts.mk
@@ -1,0 +1,14 @@
+#
+# Copyright (C) 2020 ArrowOS
+#           (C) 2022 ReloadedOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/reloaded_ginkgo.mk
+
+COMMON_LUNCH_CHOICES += \
+    reloaded_ginkgo-user \
+    reloaded_ginkgo-userdebug \
+    reloaded_ginkgo-eng

SquareTea/cafdevice_xiaomi_ginkgo

AndroidProducts.mk
@@ -1,0 +1,14 @@
+#
+# Copyright (C) 2020 ArrowOS
+#           (C) 2022 ReloadedOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/ginkgo.mk
+
+COMMON_LUNCH_CHOICES += \
+    ginkgo-user \
+    ginkgo-userdebug \
+    ginkgo-eng